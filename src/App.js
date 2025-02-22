83a039b57ef4d2dac71d431ba16bb359fccaba41
D <htehran333@gmail.com>
2025-02-22 18:06:46 -0500

    We are so Back

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Home, Layout } from "./components/Portal";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />}></Route>
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
