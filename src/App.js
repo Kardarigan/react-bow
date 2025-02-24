8b491113182ab832e7a22228c8406787b4400194
D <htehran333@gmail.com>
2025-02-24 18:04:48 -0500

    Installing Dependencies

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { Home, Layout } from "./components/Portal";
+import { Layout } from "./components";
 
 function App() {
   return (
