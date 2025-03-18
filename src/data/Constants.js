b0afcf4fd9585c529f0528e0278f65bd96b02bde
D <htehran333@gmail.com>
2025-03-18 13:00:23 +0330

    Navbar P1

---
+++ src/data/Constants.js
@@ -0,0 +1,7 @@
+import logo_only from "../asset/layout/logo.png";
+import logo_title from "../asset/layout/logo-title.png";
+
+export const Logo = {
+  icon: logo_only,
+  icon_title: logo_title,
+};
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./style/main.scss";
+import "./style/tailwind.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 const root = ReactDOM.createRoot(document.getElementById("root"));
