e820c3f76280f8081b573618b4cbfcd7db1c6908
D <htehran333@gmail.com>
2025-04-13 18:39:08 -0400

    Product Card P4

--- src/components/products/ProductCard.jsx
+++ src/components/products/ProductCard.jsx
@@ -3,22 +3,24 @@ import { Link } from "react-router-dom";
 
 const ProductCard = ({ thing }) => {
   return (
-    <div className="p-3 border-2 border-rose-500" dir="rtl">
-      <div className="h-80 overflow-hidden rounded-2xl">
+    <div className="w-64 p-3 border-2" dir="rtl">
+      <div className="h-72 overflow-hidden rounded-2xl">
         <Link>
-          <img src={thing.covers[0]} alt={thing.title + " cover"} />
+          <img
+            src={thing.covers[0]}
+            alt={thing.title + " cover"}
+            className="w-full"
+          />
         </Link>
       </div>
       <div className="w-full">
-        <div className="py-3 title-sm text-start">
-          <h3>{thing.title}</h3>
-        </div>
-        <div className=""></div>
+        <h3 className="py-3 title-sm text-start">{thing.title}</h3>
+        <div className="bg-gradient-to-r from-slate-500 to-transparent opacity-50 h-[2px] w-2/5 mr-auto"></div>
         <div className="flex-seperate">
-          <button className="button button-rose">
+          <button className="button button-rose px-5">
             <i className="fa-solid fa-plus" />
           </button>
-          <h4>
+          <h4 className="text-sm">
             <span className="title me-2">{thing.price}</span>تومان
           </h4>
         </div>
