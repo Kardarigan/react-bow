33fee2ea98831498e01e1aba0bfd0b5cbc187006
D <htehran333@gmail.com>
2025-08-09 18:15:41 -0400

    Products - last item's width fixed

--- src/components/products/ProductsFeed.jsx
+++ src/components/products/ProductsFeed.jsx
@@ -4,7 +4,7 @@ const ProductsFeed = ({ products }) => {
   const screenWidth = useWindowWidth();
 
   return (
-    <section className="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-y-5 mt-6">
+    <section className="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-y-5 mt-6">
       {products.map((product, idx) => {
         const index = idx + 1;
         let isLastItem = false;
@@ -15,7 +15,10 @@ const ProductsFeed = ({ products }) => {
         else if (screenWidth < 768 && index % 2 === 0) isLastItem = true;
 
         return (
-          <div className="flex" key={product.id || idx}>
+          <div
+            className={`flex ${isLastItem ? "pe-8" : ""}`}
+            key={product.id || idx}
+          >
             <ProductCard product={product} feed={true} />
             <div
               className={`border-2 border-rose-300 mx-5 ${
